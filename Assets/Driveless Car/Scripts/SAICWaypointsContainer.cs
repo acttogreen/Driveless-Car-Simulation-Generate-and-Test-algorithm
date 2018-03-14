e0bda91911681024fc90c1dd09e8a08e77797e12
Satriyo Aji Nugroho <acttogreen@gmail.com>
2018-03-14 07:11:37 +0700

    Update SAICWaypointsContainer.cs

--- Assets/Driveless Car/Scripts/SAICWaypointsContainer.cs
+++ Assets/Driveless Car/Scripts/SAICWaypointsContainer.cs
@@ -1,10 +1,4 @@
-﻿//----------------------------------------------
-//                  Smart AI Car
-//
-// Copyright © 2015 BoneCracker Games
-// http://www.bonecrackergames.com
-//
-//----------------------------------------------
+
 
 using UnityEngine;
 using System.Collections;
